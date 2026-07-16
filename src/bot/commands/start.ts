2c1c58a0fe46c54ba8dfd338c2ee172abc415505
speedxzpdev <speedkkzgui@gmail.com>
2026-07-16 18:09:12 -0300

    feat(start): adicionar comando de início com mensagem de boas-vindas e imagem

---
+++ src/bot/commands/start.ts
@@ -0,0 +1,32 @@
+import type Command from "@commands";
+import { Telegraf, Markup, Input } from "telegraf";
+import path from "path";
+
+const start: Command = {
+    name: "start",
+    description: "Veja mais sobre o bot",
+    usage: "/start",
+    category: "help",
+    run: async (ctx) => {
+        try {
+        const text: string = `💙 Olá ${ctx.from.first_name}! Me chamo Yuzuki, e consigo baixar videos pra você. Basta usar o nome do comando junto do link, EX: */tiktok https://www.tiktok.com/xxx*`
+
+        const buttons = Markup.inlineKeyboard([
+            Markup.button.url("😾Github", "https://github.com/speedxzpdev/yuzuki-bot"),
+        ]);
+
+        const imagePath = path.join(process.cwd(), "/src/assets/img/start.jpg")
+
+        await ctx.replyWithPhoto(Input.fromLocalFile(imagePath), {
+            parse_mode: "Markdown",
+            caption: text,
+            ...buttons
+        })
+    } catch (error) {
+        console.error(error);
+        
+    }
+    }
+}
+
+export default start
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ dotenv.config();
 import { bot, commandLoader } from "./bot/bot.js";
 import redis from "@redis"
 
+//DECIDIMOS PRIORIZAR O IPV4 POIS ALGUNS SERVIÇOS DE HOSPEDAGEM NÃO SUPORTAM IPV6, O QUE PODE CAUSAR PROBLEMAS DE CONECTIVIDADE. ALÉM DISSO, O IPV4 É MAIS AMPLAMENTE SUPORTADO E COMPATÍVEL COM A MAIORIA DAS REDES E DISPOSITIVOS. AO FORÇAR O USO DO IPV4, GARANTIMOS UMA EXPERIÊNCIA MAIS ESTÁVEL E CONFIÁVEL PARA OS USUÁRIOS, EVITANDO POSSÍVEIS INTERRUPÇÕES OU ERROS DE CONEXÃO RELACIONADOS AO IPV6.
+import dns from "node:dns";
+dns.setDefaultResultOrder("ipv4first");
+
 
 async function main() {
     console.log("inicializando api...");
