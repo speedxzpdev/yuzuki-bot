94b5791fcb0bee776f98acdd1ab4a13a2c923f4f
speedxzpdev <speedkzzgui@gmail.com>
2026-09-05 23:51:13 -0300

    refactor(redis): improve logging and remove unnecessary options

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,26 +1,34 @@
 import { Redis } from "ioredis";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const redisUri = process.env.REDIS_URI;
 
 if (!redisUri) {
-throw new Error("REDIS_URI não está definido nas variáveis de ambiente.");
+    throw new Error("REDIS_URI não está definido nas variáveis de ambiente.");
 }
 
-const redis = new Redis(redisUri, {
-    maxRetriesPerRequest: 1,
-    tls: {
-        rejectUnauthorized: false,
-    }
-});
+const redis = new Redis(redisUri);
 
 redis.on("connect", () => {
-    console.log("Conectado ao Redis com sucesso!");
+    console.log("Redis: CONNECT");
+});
+
+redis.on("ready", () => {
+    console.log("Redis: READY");
+});
+
+redis.on("close", () => {
+    console.log("Redis: CLOSE");
+});
+
+redis.on("reconnecting", () => {
+    console.log("Redis: RECONNECTING");
 });
 
 redis.on("error", (err) => {
-    console.error("Erro na conexão com o Redis:", err);
+    console.error("Redis: ERROR", err);
 });
 
 export default redis;
